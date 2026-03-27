6c1c981e58b5a851ea629a7f37391646534a1562
Aniket Jawale <94923584+aniket2001jawale@users.noreply.github.com>
2026-03-27 10:16:05 +0530

    disply_function

---
+++ displayhelp.cpp
@@ -0,0 +1,30 @@
+#include "cvsf.h"
+
+///////////////////////////////////////////////////////////////////////////
+//
+//      Function name: DisplayHelp()
+//      Description:   it is used to display help page
+//      Author:        Aniket Dnyaneshwar Jawale
+//      Date:          14/01/2026
+//
+//
+///////////////////////////////////////////////////////////////////////////
+
+void DisplayHelp()
+{
+    printf("----------------------------------------------------------------\n");
+    printf("----------------Marvellous CVSF help Page-----------------------\n");
+    printf("----------------------------------------------------------------\n");
+
+    printf("man    : it is used to display manual page\n");
+    printf("ls     : it is used to list the name of the files\n");
+    printf("clear  : is is used to clear the terminal\n");
+    printf("creat  : it is used to create new file\n");
+    printf("write  : it is used to write the data into the file\n");
+    printf("read   : it is used to read the data from the file\n");
+    printf("stat   : is is used to display stastical information\n");
+    printf("unlink : it is used to delete the file\n");
+    printf("exit   : it is used to terminate marvellous CVSF\n");
+
+    printf("----------------------------------------------------------------\n");
+}
